c937f19f3a0d37a8dd1d557d11425c81a357200f
menegass0 <victormenegasso@unisantos.br>
2023-02-10 18:17:26 -0300

    eye-icon-password

--- javascript/pass-show-hide.js
+++ javascript/pass-show-hide.js
@@ -4,8 +4,10 @@ toggleBtn = document.querySelector(".form .field i");
 toggleBtn.onclick = () =>{
     if(paswdField.type == "password"){
         paswdField.type = "type";
+        toggleBtn.classList.add("active");
     }
     else{
         paswdField.type = "password";
+        toggleBtn.classList.remove("active");
     }
 }
