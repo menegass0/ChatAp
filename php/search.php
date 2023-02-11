53e25f8064fde243d725ed0f59b66ab0ab75f98b
menegass0 <victormenegasso@unisantos.br>
2023-02-11 10:30:07 -0300

    fixin-search

--- php/search.php
+++ php/search.php
@@ -1,6 +1,7 @@
 <?php
-    include_once "config.php";
     session_start();
+    include_once "config.php";
+    $outgoing_id = $_SESSION['unique_id'];
     $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
     $output = '';
     $sql = mysqli_query($conn, "SELECT * FROM users WHERE fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%' OR CONCAT(fname, ' ', lname) LIKE '%{$searchTerm}%' EXCEPT SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
