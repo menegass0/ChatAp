92169847c0e14a5f07d2e9762878aeb933767f25
menegass0 <victormenegasso@unisantos.br>
2023-02-10 22:10:28 -0300

    image-file-manipulation

--- php/signup.php
+++ php/signup.php
@@ -11,8 +11,29 @@
             if(mysqli_num_rows()>0){
                 echo "$email - This email already exist!";
             }else{
-                if(isset($_FILES['file'])){
-                    
+                if(isset($_FILES['image'])){
+                    $img_name = $_FILES['image']['name'];
+                    $img_type = $_FILES['image']['type'];
+                    $tmp_name = $_FILES['image']['tmp_name'];
+
+                    $img_explode = explode('.' $img_name);
+                    $img_ext = end($img_explode);
+
+                    $extentions = ['png', 'jpeg', 'jpg'];
+                    if(in_array($img_ext, $extentions) === true){
+                        $time = time();
+
+                        $new_img_name = $time.$img_name;
+                        if(move_uploaded_file($tmp_name, "images/".$new_img_name)){
+                            $status = "Active now";
+                            $random_id =  ran(time(), 10000000);
+
+                            
+                        }
+                        
+                    }else{
+                        echo "Please select an Image file - jpeg, jpg, png!"
+                    }
                 }else{
                     echo "Please select an image file"
                 }
