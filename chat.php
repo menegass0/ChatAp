c688443cfebd5250ffc3fdd7b60c41768c3e1162
menegass0 <victormenegasso@unisantos.br>
2023-02-11 07:16:06 -0300

    chat-with-other-user

--- chat.php
+++ chat.php
@@ -1,15 +1,27 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['unique_id'])){
+        header("location: login.php");
+    }
+?>
 <?php include_once "header.php"; ?>
 <body>
     <div class="wrapper">
         <section class="chat-area">
             <header>
-                <a href="" class="back-icon">
-                    <i class="fas fa-arrow-left"></i>
-                </a>
-                <img src="pato.jpg" alt="">
+                <?php
+                    include_once "php/config.php";
+                    $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
+                    $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}");
+                    if(mysqli_num_rows($sql)>0){
+                        $row = mysqli_fetch_assoc($sql);
+                    }
+                ?>
+                <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <img src="php/images/<?php echo $row['img']?>" alt="">
                 <div class="details">
-                    <span>Jaozin Farofa</span>
-                    <p>Active now</p>
+                    <span><?php echo $row['fname']." ".$row['lname']; ?></span>
+                    <p><?php echo $row['status']?></p>
                 </div>
             </header>
             <div class="chat-box">
@@ -60,10 +72,14 @@
                 </div>
             </div>
             <form action="#" class="typing-area">
-                <input type="text" placeholder="Type a message here...">
+                <input type="hidden" name="outgoing_id" value="<?php echo $_SESSION['unique_id']?>">
+                <input type="hidden" name="incoming_id" value="<?php echo $user_id?>">
+                <input type="text" name="message" class="input-field" placeholder="Type a message here...">
                 <button><i class="fab fa-telegram-plane"></i></button>
             </form>
         </section>
     </div>
+
+    <script src="javascript/chat.js"></script>
 </body>
 </html>
---
+++ javascript/chat.js
@@ -0,0 +1,23 @@
+const form = document.querySelector(".typing-area"),
+inputField = form.querySelector(".input-field"),
+sendBtn = form.querySelector("button");
+
+sendBtn.onclick = ()=>{
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/chat.php", true);
+    xhr.onload = ()=>{
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                let data = xhr.response;
+                if(data == "Success!"){
+                    location.href = "users.php";
+                }else{
+                    errorText.textContent = data;
+                    errorText.style.display = "block";
+                }
+            }
+        }
+    }
+    let formData = new FormData(form);
+    xhr.send(formData);
+}
