7df3f0774db79092592580bae7192252aba8c3e3
menegass0 <victormenegasso@unisantos.br>
2023-02-11 01:06:00 -0300

    ajax-onkeyup-search

--- javascript/users.js
+++ javascript/users.js
@@ -8,6 +8,22 @@ searchBtn.onclick = ()=>{
     searchBtn.classList.toggle("active");
 }
 
+searchBar.onkeyup = ()=>{
+    let searchTerm = searchBar.value;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/search.php", true);
+    xhr.onload = ()=>{
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                let data = xhr.response;
+                usersList.innerHTML = data;
+            }
+        }
+    }
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("searchTerm=" + searchTerm);
+}
 
 setInterval(()=>{
     let xhr = new XMLHttpRequest();
@@ -16,7 +32,6 @@ setInterval(()=>{
         if(xhr.readyState === XMLHttpRequest.DONE){
             if(xhr.status === 200){
                 let data = xhr.response;
-                console.log(data);
                 usersList.innerHTML = data;
             }
         }
--- php/users.php
+++ php/users.php
@@ -7,18 +7,7 @@
     if(mysqli_num_rows($sql) == 1){
         $output .= "No users are avaliable to chat";
     }elseif(mysqli_num_rows($sql)>0){
-        while($row = mysqli_fetch_assoc($sql)){
-            $output .= '<a href="#">
-                        <div class="content">
-                        <img src="php/images/'.$row['img'].'" alt="">
-                        <div class="details">
-                            <span>'.$row['fname'].' '.$row['lname'].'</span>
-                            <p>This is test message</p>
-                        </div>
-                        </div>
-                        <div class="status-dot"><i class="fas fa-circle"></i></div>
-                        </a>';
-        }
+        include "data.php";
     }
     echo $output
 ?>
