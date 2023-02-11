87d7d6788ea52d32ec8023f27d54d810cc665ec7
menegass0 <victormenegasso@unisantos.br>
2023-02-10 23:43:05 -0300

    user-informations-php

--- users.php
+++ users.php
@@ -1,16 +1,27 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['unique_id'])){
+        header("location: login.php");
+    }
+?>
+
 <?php include_once "header.php"; ?>
 <body>
     <div class="wrapper">
         <section class="users">
             <header>
             <?php
-
+                include_once "php/config.php";
+                $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+                if(mysqli_num_rows($sql)>0){
+                    $row = mysqli_fetch_assoc($sql);
+                }
             ?>
                 <div class="content">
-                    <img src="pato.jpg" alt="">
+                    <img src="php/images/<?php echo $row['img']?>" alt="">
                     <div class="details">
-                        <span>Jaozin Farofa</span>
-                        <p>Active now</p>
+                        <span><?php echo $row['fname']." ".$row['lname']; ?></span>
+                        <p><?php echo $row['status']?></p>
                     </div>
                 </div>
                 <a href="#" class="logout">Logout</a>
