e8d0f661041ffb2646ecb070d69ae0b9865b6a03
menegass0 <victormenegasso@unisantos.br>
2023-02-10 23:17:42 -0300

    header.php

--- chat.php
+++ chat.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://kit.fontawesome.com/e6f0797e64.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">
-</head>
+<?php include_once "header.php"; ?>
 <body>
     <div class="wrapper">
         <section class="chat-area">
--- users.php
+++ users.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://kit.fontawesome.com/e6f0797e64.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">
-</head>
+<?php include_once "header.php"; ?>
 <body>
     <div class="wrapper">
         <section class="users">
             <header>
+            <?php
+
+            ?>
                 <div class="content">
                     <img src="pato.jpg" alt="">
                     <div class="details">
