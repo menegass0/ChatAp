ae83883c0172d880318cdf44886ffb934740be48
menegass0 <victormenegasso@unisantos.br>
2023-02-11 15:32:52 -0300

    fixing-stuff

--- php/users.php
+++ php/users.php
@@ -5,10 +5,10 @@
     $sql = mysqli_query($conn , "SELECT * FROM users EXCEPT SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']} ORDER BY status");
     $output = "";
 
-    if(mysqli_num_rows($sql) == 1){
-        $output .= "No users are avaliable to chat";
-    }elseif(mysqli_num_rows($sql)>0){
+    if(mysqli_num_rows($sql)>0){
         include "data.php";
+    }else{
+        $output .= "No users are avaliable to chat";
     }
     echo $output
 ?>
