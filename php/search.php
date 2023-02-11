5ff385fbc13c65027b24e363762f70c124b8c728
menegass0 <victormenegasso@unisantos.br>
2023-02-11 01:27:50 -0300

    prevents-user-from-showing-in-his-own-searchbar

--- php/search.php
+++ php/search.php
@@ -1,8 +1,9 @@
 <?php
     include_once "config.php";
+    session_start();
     $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
     $output = '';
-    $sql = mysqli_query($conn, "SELECT * FROM users WHERE fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%' OR CONCAT(fname, ' ', lname) LIKE '%{$searchTerm}%'");
+    $sql = mysqli_query($conn, "SELECT * FROM users WHERE fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%' OR CONCAT(fname, ' ', lname) LIKE '%{$searchTerm}%' EXCEPT SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
     if(mysqli_num_rows($sql) > 0){
         include "data.php";
     }else{
