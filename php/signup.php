4ffb2bddd1343295f4bd63fe0176b78348926f75
menegass0 <victormenegasso@unisantos.br>
2023-02-10 22:36:39 -0300

    insertion-into-users-table

--- php/signup.php
+++ php/signup.php
@@ -8,15 +8,14 @@
     if(!empty($fname) && !empty($lname) && !empty($email) && !empty($password)){
         if(filter_var($email, FILTER_VALIDATE_EMAIL)){
             $sql = mysqli_query($conn, "SELECT email FROM users WHERE email = '{$email}'");
-            if(mysqli_num_rows()>0){
+            if(mysqli_num_rows($sql)>0){
                 echo "$email - This email already exist!";
             }else{
                 if(isset($_FILES['image'])){
                     $img_name = $_FILES['image']['name'];
-                    $img_type = $_FILES['image']['type'];
                     $tmp_name = $_FILES['image']['tmp_name'];
 
-                    $img_explode = explode('.' $img_name);
+                    $img_explode = explode('.', $img_name);
                     $img_ext = end($img_explode);
 
                     $extentions = ['png', 'jpeg', 'jpg'];
@@ -26,16 +25,27 @@
                         $new_img_name = $time.$img_name;
                         if(move_uploaded_file($tmp_name, "images/".$new_img_name)){
                             $status = "Active now";
-                            $random_id =  ran(time(), 10000000);
+                            $random_id =  rand(time(), 10000000);
 
-                            
+                            $sql2 = mysqli_query($conn, "INSERT INTO users (unique_id, fname, lname, email, password, img, status) VALUES ({$random_id}, '{$fname}', '{$lname}', '{$email}', '{$password}', '{$new_img_name}', '{$status}' )");
+                            if($sql){
+                                $sql3 = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
+                                if(mysqli_num_rows($sql3) > 0){
+                                    $row = mysqli_fetch_assoc($sql3);
+                                    $_SESSION['unique_id'] = $row['unique_id']; //putting unique id into session so we can use it in other php files
+                                    echo "Success!";
+                                }
+                            }
+                            else{
+                                echo "Something went wrong!";
+                            }
                         }
                         
                     }else{
-                        echo "Please select an Image file - jpeg, jpg, png!"
+                        echo "Please select an Image file - jpeg, jpg, png!";
                     }
                 }else{
-                    echo "Please select an image file"
+                    echo "Please select an image file";
                 }
             }
             
