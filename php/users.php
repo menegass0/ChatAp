6cef8d36654485af3865b3d8224d35ab0d896779
menegass0 <victormenegasso@unisantos.br>
2023-02-11 01:24:33 -0300

    dont-show-the-the-session-user-in-the-user-list

--- php/users.php
+++ php/users.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     include_once "config.php";
-    $sql = mysqli_query($conn , "SELECT * FROM users");
+    $sql = mysqli_query($conn , "SELECT * FROM users EXCEPT SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
     $output = "";
 
     if(mysqli_num_rows($sql) == 1){
